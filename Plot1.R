c975e9dbc136f82792f84230c9e2f5b112725fae
Darren <darren.mcnally@gmail.com>
2016-06-08 13:07:40 -0700

    updating with plotting...

--- Plot1.R
+++ Plot1.R
@@ -1,17 +1,29 @@
 if (!file.exists("summarySCC_PM25.rds")) {
   url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
   download.file(url, "data.zip", method = "curl")
-  unzip("data.zip")}
+  unzip("data.zip")
+  rm(url)}
 if (!library(dplyr, logical.return = TRUE)){
   install.packages("dplyr")
   library(dplyr)}
 
-NEI <- readRDS("summarySCC_PM25.rds")
-SCC <- readRDS("Source_Classification_Code.rds")
+nei <- readRDS("summarySCC_PM25.rds")
+#scc <- readRDS("Source_Classification_Code.rds")
+
 # Have total emissions from PM2.5 decreased in the United States from 1999 to 2008?
 # Using the base plotting system, make a plot showing the total PM2.5 emission from
 # all sources for each of the years 1999, 2002, 2005, and 2008.
-t <- NEI %>% 
+
+t <- nei %>% 
   group_by(year) %>% 
   summarize(total.pm25=sum(Emissions))
 
+# Open the device, plot the graph, and close the device.
+png("plot1.png")
+plot(total.pm25~year, data=t,
+     main="Total PM2.5 Emissions by Year",
+     ylab="Total PM2.5 Emmsions",
+     xlab="Year",
+     pch=16)
+abline(lm(total.pm25~year, data=t))
+dev.off()
