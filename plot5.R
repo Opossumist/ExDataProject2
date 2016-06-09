184ae294f5791dc0e220b939203e8b50da090236
Darren <darren.mcnally@gmail.com>
2016-06-08 20:06:14 -0700

    Finishing up with plot 4

--- plot5.R
+++ plot5.R
@@ -10,27 +10,26 @@ if (!library(ggplot2, logical.return = TRUE)){
   install.packages("dplyr")
   library(ggplot2)}
 
+# How have emissions from motor vehicle sources changed from 1999–2008 in Baltimore City?
 nei <- readRDS("summarySCC_PM25.rds")
 scc <- readRDS("Source_Classification_Code.rds")
 
-# Across the United States, how have emissions from coal combustion-related sources
-# changed from 1999–2008?
-
-# Find SCC code for coal sources
-coal <- scc[grep("Coal", scc$EI.Sector), c("SCC", "EI.Sector")]
-coal$SCC <- as.character(coal$SCC)
-coal <- as.character(coal$SCC)
+# Find SCC code for all vehicle sources
+mv <- scc[grep("^Mobile", scc$EI.Sector), c("SCC", "EI.Sector")]
+mv <- as.character(mv$SCC)
 
 # Isolate data for sources from coal
-tcoal <- nei[nei$SCC %in% coal,] %>%
+tmv <- nei[(nei$SCC %in% mv & nei$fips =="24510"),] %>%
   group_by(year) %>%
   summarize(total.pm25=sum(Emissions))
+rm(mv, nei, scc)
 
 # Construct and save graph
-g <- ggplot(data=tcoal, aes(year, total.pm25))
+g <- ggplot(data=tmv, aes(year, total.pm25))
 g <- g + geom_point()
 g <- g + geom_smooth(method="lm")
 g <- g + labs(x="Year", y="Total Emissions (tons)")
-g <- g + ggtitle("National PM2.5 Trend for Coal")
-ggsave("plot4.png", plot=g, device="png")
-rm(nei, neicoal, scc, coal, tcoal, g)
+g <- g + scale_y_continuous(limits=c(0,NA))
+g <- g + ggtitle("Baltimore PM2.5 Trend for All Mobile Sources")
+ggsave("plot5.png", plot=g, device="png")
+rm(tmv, g)
